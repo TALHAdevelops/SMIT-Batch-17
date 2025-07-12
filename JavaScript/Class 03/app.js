4d2418ade1d559d06936b2c8136bfaf2dc8a9209
AdilAhmedShekhani <126003246+AdilAhmedShekhani@users.noreply.github.com>
2025-07-12 10:20:06 +0500

    Concating

--- JavaScript/Class 03/app.js
+++ JavaScript/Class 03/app.js
@@ -1,4 +1,4 @@
-alert("Hello Students");
+// alert("Hello Students");
 
 // var num = 10;
 
@@ -17,3 +17,34 @@ alert("Hello Students");
 
 // num--; => Post Decrement
 // --num; => Pre Decrement
+
+// Using HTML in Javascript
+// document.write("<h1> Hello Students </h1>");
+
+// Rule of JavaScript
+
+// Undefind and NotDefine
+
+// Undefined => Variable Create but no value define
+// Notdefined => Variable not created
+
+// var total = (2 + 3) * 4 => Javascript First Solve Bracket then Multiplication
+// console.log(total)
+
+// var total = 2 + 3 * 4 => Javascript First Solve Multiplication the Addition
+// console.log(total)
+
+// Practice Question of Pre / Post Increment and Decrement
+
+// var a = 2;
+// var b = 3;
+// c = a++ - b-- + --a + a++;
+// console.log(c);
+
+// Concating in Javascript
+
+// var num = 2 + "2";
+// console.log(num);
+
+// Type of
+// console.log(typeof num);
