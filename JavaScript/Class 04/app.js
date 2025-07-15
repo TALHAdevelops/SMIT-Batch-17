682d39adcffb61c103e098a08b7ed133c171525d
AdilAhmedShekhani <adilaziz2009@live.com>
2025-07-15 22:18:42 +0500

    Prompt

--- JavaScript/Class 04/app.js
+++ JavaScript/Class 04/app.js
@@ -1,1 +1,20 @@
-alert("Hello")
+alert("Hello");
+
+// Prompts
+
+var fullName = prompt();
+
+// Place Holder
+
+var spec = prompt("Your species?", "human");
+
+// Place Holder Using Variables
+var question = "Your species?";
+var defaultAnswer = "human";
+var spec = prompt(question, defaultAnswer);
+
+// Response in String
+var numberOfCats = prompt("How many cats?");
+var tooManyCats = numberOfCats + 1;
+console.log(tooManyCats)
+
