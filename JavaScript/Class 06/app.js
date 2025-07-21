07e9c093a8f46d68a5d41a7a923f397fd187775a
AdilAhmedShekhani <adilaziz2009@live.com>
2025-07-21 22:37:17 +0500

    if...else and else if statements

--- JavaScript/Class 06/app.js
+++ JavaScript/Class 06/app.js
@@ -1,2 +1,27 @@
 console.log("Hello Students");
 
+// If Statements
+// var x = prompt("Where does the Pope live?");
+// if (x === "Vatican") {
+// alert("Correct!");
+// }
+
+// if (age <= 20) {
+//   console.log("Eligible");
+// } else {
+//   console.log("Not Eligible");
+// }
+
+// IF ELSE IF STATEMENT
+
+// var kuchLanahai = prompt("Kia mil raha hai ");
+
+// if (kuchLanahai === "Dahi") {
+//   console.log("le ana");
+// } else if (kuchLanahai === "Dhoodh") {
+//   console.log("Acha Le aao");
+// } else if (kuchLanahai === "Bread") {
+//   console.log("bhai le aao yar");
+// } else {
+//   console.log("Bhai kuch bhi le aao");
+// }
