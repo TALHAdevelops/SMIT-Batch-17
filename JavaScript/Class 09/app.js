847de4ed569ee5598cb84e0cde3696b515e3d075
AdilAhmedShekhani <adilaziz2009@live.com>
2025-07-30 22:21:00 +0500

    Lower & Upper Case Methods

--- JavaScript/Class 09/app.js
+++ JavaScript/Class 09/app.js
@@ -1,1 +1,14 @@
 console.log("Hello Students");
+
+// Upper Case and Lower Case Method
+
+// var city = prompt("What is your City Name ?")
+
+// if (city.toLowerCase === "karachi") {
+//     alert("Welcome to city of Lights")
+// }
+// var city = prompt("What is your City Name ?")
+
+// if (city.toUpperCase === "KARACHI") {
+//     alert("Welcome to city of Lights")
+// }
