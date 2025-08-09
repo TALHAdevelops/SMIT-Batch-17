0689893997f9bd0f5c2eadf7d0d04b8f8cddf9eb
AdilAhmedShekhani <126003246+AdilAhmedShekhani@users.noreply.github.com>
2025-08-09 14:14:01 +0500

    Change Case

--- JavaScript/Class 12/app.js
+++ JavaScript/Class 12/app.js
@@ -45,3 +45,20 @@ for (var i = 0; i < firstNames.length; i++) {
 // Adil Choudry
 // Rizwan Choudry
 // Sufiyan Choudry
+
+// Changing Case (LowerCase / UpperCase)
+
+var cityToCheck = prompt("Enter your city");
+cityToCheck = cityToCheck.toLowerCase();
+var cleanestCities = [
+  "cheyenne",
+  "santa fe",
+  "tucson",
+  "great falls",
+  "honolulu",
+];
+for (var i = 0; i <= 4; i++) {
+  if (cityToCheck === cleanestCities[i]) {
+    alert("It's one of the cleanest cities");
+  }
+}
