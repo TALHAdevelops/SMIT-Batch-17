772cb0bcd6a2164a00985d2bf46edde05de6390e
AdilAhmedShekhani <adilaziz2009@live.com>
2025-08-25 22:20:00 +0500

    Convert String to Integers

--- JavaScript/Class 15/app.js
+++ JavaScript/Class 15/app.js
@@ -1,11 +1,31 @@
 alert("Hello")
 
+// Math Random Method
 var randomNumber = Math.random();
 
 console.log(randomNumber)
 
+// Math Random and Math Floor Methods
 var bigDecimal = Math.random();
 var improvedNum = (bigDecimal * 6) + 1;
 var numberOfStars = Math.floor(improvedNum);
 
 console.log(numberOfStars)
+
+// Convert String to integers & Decimal
+
+var currentAge = prompt("Enter your age.");
+var yearsEligibleToVote = currentAge - 18;
+
+console.log(yearsEligibleToVote)
+
+var currentAge = prompt("Enter your age.");
+var qualifyingAge = parseInt(currentAge) + 1;
+
+console.log(qualifyingAge)
+
+// Parse Float
+
+var myFractional = parseFloat("1.9999");
+
+console.log(myFractional)
