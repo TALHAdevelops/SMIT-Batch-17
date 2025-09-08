0d29db6bbe90388a7d6b2c821261b3ee09d06782
AdilAhmedShekhani <adilaziz2009@live.com>
2025-09-08 22:54:46 +0500

    Getting the current date and time

--- JavaScript/Class 18/app.js
+++ JavaScript/Class 18/app.js
@@ -1,1 +1,20 @@
 console.log("Hello Students")
+
+var rightNow = new Date();
+
+console.log(rightNow)
+
+// Change in String
+var dateString = rightNow.toString();
+
+console.log(dateString)
+
+// Day Method
+
+var dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+var rightNow = new Date();
+var theDay = rightNow.getDay();
+var nameOfToday = dayNames[theDay];
+
+
+console.log(nameOfToday)
