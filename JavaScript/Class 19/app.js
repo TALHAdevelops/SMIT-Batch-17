32fac1c8bf1901fcf3a7930cb684545f25c6b838
AdilAhmedShekhani <adilaziz2009@live.com>
2025-09-08 23:10:22 +0500

    Changing elements of a date and time

---
+++ JavaScript/Class 19/app.js
@@ -0,0 +1,29 @@
+// Set Full Year
+var d = new Date();
+d.setFullYear(2001);
+
+// Set Month
+
+var d = new Date();
+d.setMonth(11);
+
+// Set Date
+
+var d = new Date();
+d.setDate(15);
+
+// Set Hours
+var d = new Date();
+d.setHours(13);
+
+// Set Minutes
+var d = new Date();
+d.setMinutes(5);
+
+// Set Seconds
+var d = new Date();
+d.setSeconds(55);
+
+// Set Miliseconds
+var d = new Date();
+d.setMilliseconds(867);
