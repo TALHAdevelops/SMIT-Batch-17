51ae479ca7b0a1ba0f61fab270a3cd9f4644fd11
AdilAhmedShekhani <126003246+AdilAhmedShekhani@users.noreply.github.com>
2025-09-13 11:31:11 +0500

    Data Passing in Function

--- JavaScript/Class 20/app.js
+++ JavaScript/Class 20/app.js
@@ -1,21 +1,39 @@
-console.log("Hello")
+console.log("Hello");
 
-// Basic Structure of Function 
+// Basic Structure of Function
 
 function functionName(parameter1, parameter2) {
-    // function ke andar ka code (body)
+  // function ke andar ka code (body)
 }
 
-// Function Call 
+// Function Call
 
-functionName(Argument1, Arguments2)
+functionName(Argument1, Arguments2);
 
 // Example
 
 function greet(name) {
-    console.log("Hello, " + name + "!");
+  console.log("Hello, " + name + "!");
 }
 
-// Call Function 
+// Call Function
 
-greet("Ali");  // Output: Hello, Ali!
+greet("Ali"); // Output: Hello, Ali!
+
+// Passing Data through Variable
+
+function greetUser(greeting) {
+  alert(greeting);
+}
+
+var whatever = "Hello, there.";
+greetUser(whatever);
+
+// Passing Data (String,Number)
+
+function showMessage(m, string, num) {
+  alert(m + string + num);
+}
+
+var month = "March";
+showMessage(month, "'s winner number is ", 23);
