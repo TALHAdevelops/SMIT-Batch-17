5ad479bd728c17ad9c08e6dd51121f45daaccdfb
AdilAhmedShekhani <126003246+AdilAhmedShekhani@users.noreply.github.com>
2025-09-13 11:21:18 +0500

    Basic Structure Function

--- JavaScript/Class 20/app.js
+++ JavaScript/Class 20/app.js
@@ -1,1 +1,12 @@
 console.log("Hello")
+
+// Basic Structure of Function 
+
+function functionName(parameter1, parameter2) {
+    // function ke andar ka code (body)
+}
+
+// Function Call 
+
+functionName(Argument1, Arguments2)
+
