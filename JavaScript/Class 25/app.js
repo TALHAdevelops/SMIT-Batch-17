e2a389adc5c3b4a21b478ff307620e86d8294a60
AdilAhmedShekhani <adilaziz2009@live.com>
2025-10-02 22:42:19 +0500

    ChatBot

--- JavaScript/Class 25/app.js
+++ JavaScript/Class 25/app.js
@@ -16,3 +16,41 @@ function fillCity() {
             console.log("Koi Value Nhi")
     }
 }
+
+var list = document.getElementById("list")
+var typing = document.getElementById("typing")
+
+function sendMessage() {
+
+    if (event.keyCode === 13) {
+        var elem = event.target;
+        list.innerHTML += "<li>" + elem.value + "</li>"
+        typing.style.display = 'block'
+        if (['hi', 'hey', 'oye'].indexOf(elem.value.toLowerCase()) !== -1) {
+            setTimeout(function () {
+
+                list.innerHTML += "<li> Hello </li>"
+            }, 2000)
+            setTimeout(function () {
+
+                list.innerHTML += "<li> Kaise Hain Aap ? </li>"
+                typing.style.display = 'none'
+            }, 4000)
+        }
+        else if (['theek', 'fine', 'fit'].indexOf(elem.value.toLowerCase()) !== -1) {
+
+            setTimeout(function () {
+
+                list.innerHTML += "<li> Me Aapki Kia Madad Kar Sakta hun ? </li>"
+                typing.style.display = 'none'
+            }, 2000)
+        } else {
+            setTimeout(function () {
+
+                list.innerHTML += "<li> Smjh nhi aaraha </li>"
+                typing.style.display = 'none'
+            }, 2000)
+        }
+        elem.value = ""
+    }
+}
