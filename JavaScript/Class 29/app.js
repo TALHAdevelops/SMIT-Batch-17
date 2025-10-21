de8dd2397486a3e80f0fb0a99a82490b47c2d5c5
AdilAhmedShekhani <126003246+AdilAhmedShekhani@users.noreply.github.com>
2025-10-21 11:22:56 +0500

    The DOM:

--- JavaScript/Class 29/app.js
+++ JavaScript/Class 29/app.js
@@ -1,1 +1,14 @@
 console.log("Hello");
+
+// Counting Elements
+var liElements = document.getElementsByTagName("li");
+var howManyLi = liElements.length;
+console.log(howManyLi);
+
+// Counting Elements (Loop)
+
+for (var i = 0; i < howManyLi; i++) {
+  if (liElements[i].innerHTML === "") {
+    liElements[i].innerHTML = "coming soon";
+  }
+}
