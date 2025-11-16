bd5b091d0a22591053d18410b9a78ee4c7cc3490
AdilAhmedShekhani <adilaziz2009@live.com>
2025-11-16 21:25:49 +0500

    Getting and setting the URL

--- JavaScript/Class 35/app.js
+++ JavaScript/Class 35/app.js
@@ -2,3 +2,12 @@ var whereWeAt = window.location.href;
 
 
 console.log(whereWeAt)
+
+var domain = window.location.href = "http://www.google.com";
+
+console.log(domain)
+
+var currentSite = window.location.hostname;
+ var currentPath = window.location.pathname;
+ var destination = "http://" + currentSite + currentPath + "#humidifier";
+ window.location.href = destination;
