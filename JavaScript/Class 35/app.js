d7347544238a8668a8086e916fba1f4bb4bfc263
AdilAhmedShekhani <adilaziz2009@live.com>
2025-11-16 21:31:48 +0500

    Forward and reverse

--- JavaScript/Class 35/app.js
+++ JavaScript/Class 35/app.js
@@ -10,15 +10,31 @@ var domain = window.location.href = "http://www.google.com";
 console.log(domain)
 
 var currentSite = window.location.hostname;
- var currentPath = window.location.pathname;
- var destination = "http://" + currentSite + currentPath + "#humidifier";
- window.location.href = destination;
+var currentPath = window.location.pathname;
+var destination = "http://" + currentSite + currentPath + "#humidifier";
+window.location.href = destination;
 
 
 // Getting and setting the URL another way
 
- window.location.assign("http://www.me.com");
+window.location.assign("http://www.me.com");
 
- window.location.reload(true);
+window.location.reload(true);
 window.location.reload(false);
 window.location.reload();
+
+// Forward and reverse
+
+history.back();
+history.forward();
+// Backspace key three times
+
+history.go(-3);
+
+// browser forward two URLs
+
+history.go(2);
+
+var whereUserCameFrom = document.referrer;
+
+console.log(whereUserCameFrom)
