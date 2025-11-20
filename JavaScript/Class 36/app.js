43f3f555e89b0e6ea2e2e67fc7c636fe2cc9ec85
AdilAhmedShekhani <adilaziz2009@live.com>
2025-11-20 21:17:07 +0500

    Try catch (using throw)

--- JavaScript/Class 36/app.js
+++ JavaScript/Class 36/app.js
@@ -14,3 +14,33 @@ function greetWorld() {
         alert(err);
     }
 }
+
+
+// Try catch (using throw)
+
+function checkPassword() {
+    try {
+        var pass = document.getElementById("f1").value;
+        if (pass.length < 8) {
+            throw "Please enter at least 8 characters.";
+        }
+        if (pass.indexOf(" ") !== -1) {
+            throw "No spaces in the password, please.";
+        }
+        var numberSomewhere = false;
+        for (var i = 0; i < pass.length; i++) {
+            if (isNaN(pass(i, i + 1)) === false) {
+                numberSomewhere = true;
+                break;
+            }
+        }
+        if (numberSomewhere === false) {
+            throw "Include at least 1 number.";
+        }
+    }
+    catch (err) {
+        alert(err);
+    }
+}
+
+
