a7df3c13421d023039ddb777b465b4a6770fcaeb
AdilAhmedShekhani <adilaziz2009@live.com>
2025-11-30 14:03:04 +0500

    Spread Operators

--- Advance Javascript/Class 02/app.js
+++ Advance Javascript/Class 02/app.js
@@ -1,17 +1,36 @@
 // Default Parameters
 
 function abc(a = "Kuch Likhenge") {
-  console.log(a);
+    console.log(a);
 }
 
-abc();        
-abc("Adil");  
+abc();
+abc("Adil");
 
 // Rest Parameters
 
 function info(name, ...hobbies) {
-  console.log(name);
-  console.log(hobbies);
+    console.log(name);
+    console.log(hobbies);
 }
 
 info("Ali", "Cricket", "Football", "Coding");
+
+// Spread Operators
+
+// Arrays
+
+let a = [1, 2];
+let b = [3, 4];
+
+let merged = [...a, ...b];
+
+console.log(merged);
+
+// Objects
+
+let student = { name: "Ali", age: 20 };
+let copyStudent = { ...student };
+
+console.log(copyStudent);
+
