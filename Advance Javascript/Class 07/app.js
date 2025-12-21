0e8f7363c070695cb95261f840a1a078e448d2b5
AdilAhmedShekhani <adilaziz2009@live.com>
2025-12-21 17:09:21 +0500

    Remove Duplicate Value

--- Advance Javascript/Class 07/app.js
+++ Advance Javascript/Class 07/app.js
@@ -10,3 +10,24 @@ mySet.add(10);
 
 console.log(mySet.has(10)); 
 console.log(mySet.has(30)); 
+
+// Delete Value
+
+mySet.delete(20);
+
+// Checking Size
+
+console.log(mySet.size); 
+
+// Clear 
+
+console.log(mySet.size); 
+
+// Remove Duplicate Value
+
+let numbers = [1, 2, 2, 3, 4, 4];
+let uniqueNumbers = new Set(numbers);
+
+console.log(uniqueNumbers);
+
+
