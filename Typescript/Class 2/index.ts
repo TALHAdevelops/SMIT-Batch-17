7d2cbac465c6057687883e4ed625b94b7df42ac1
AdilAhmedShekhani <adilaziz2009@live.com>
2026-01-18 17:50:12 +0500

    Objects Create

--- Typescript/Class 2/index.ts
+++ Typescript/Class 2/index.ts
@@ -1,3 +1,26 @@
-let abc : {
-    
-}
+// let student = {
+//   name: "Adil",
+//   age: 20,
+//   isPassed: true
+// };
+
+// let student: {
+//   name: string;
+//   age: number;
+//   isPassed: boolean;
+// } = {
+//   name: "Ali",
+//   age: 20,
+//   isPassed: true
+// };
+
+// Object with Optional Property
+
+let student: {
+  name: string;
+  age: number;
+  isPassed?: boolean;
+} = {
+  name: "Ali",
+  age: 20
+};
